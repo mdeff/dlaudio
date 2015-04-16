f0c25762b83b7c6a83e9c6e1b1e7d4431aaf61e2
Michaël Defferrard <michael.defferrard@epfl.ch>
2015-04-16 17:15:47 +0200

    Install librosa for audio processing

--- create.sh
+++ create.sh
@@ -2,4 +2,6 @@
 source /etc/bash_completion.d/virtualenvwrapper
 rm -rf ~/.virtualenvs/dlaudio
 mkvirtualenv --system-site-packages --python=python2.7 dlaudio
-pip install --upgrade pyunlocbox ipython pyzmq tornado jsonschema mistune
+pip install --upgrade pyunlocbox
+pip install --upgrade ipython pyzmq tornado jsonschema mistune
+pip install --upgrade librosa scikits.samplerate
