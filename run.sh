bf35126bcd4e7002fed312f241e359a20397c697
Michaël Defferrard <michael.defferrard@epfl.ch>
2015-06-03 09:10:58 +0000

    run: log ipython outputs

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ source /etc/bash_completion.d/virtualenvwrapper
 workon dlaudio
 
 # Execute via IPython (default timeout is 30s per cell).
-ipython nbconvert --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=1000000 $1
+ipython nbconvert --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=1000000 $1 > log.txt 2>&1
 
 # Execute via Python.
 #ipython3 nbconvert --to python $1
