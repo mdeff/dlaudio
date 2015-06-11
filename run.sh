62d9d8cabd15e9410417816b97fdccb50a585959
Michaël Defferrard <michael.defferrard@epfl.ch>
2015-06-11 15:14:44 +0000

    run: allow to save results as notebook, not only html

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# Parameters:
+# 1) Name of the notebook.
+# 2) Output format, e.g. html or notebook.
+
 source /etc/bash_completion.d/virtualenvwrapper
 workon dlaudio
 
 # Execute via IPython (default timeout is 30s per cell).
-ipython nbconvert --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=1000000 $1 > log.txt 2>&1
+ipython nbconvert --to=$2 --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=1000000 $1 > log.txt 2>&1
 
 # Execute via Python.
 #ipython3 nbconvert --to python $1
